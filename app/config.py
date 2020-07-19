85cf1120896fbdd0ec0fa5e07e4dbde948894e4e
OMAR <omo9@mail.com>
2020-07-19 02:48:59 +0200

    add Procfile

--- app/config.py
+++ app/config.py
@@ -7,5 +7,7 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECCRET_KEY') or 'admin'
 
     MONGODB_SETTINGS = {
-        'db': os.environ.get("MONGO_NAME") or "manzel_v1_test"
+        "db": os.environ.get("MONGODB_NAME") or "manzel_v1_test",
+        "host": os.environ.get("MONGODB_URI"),
+        "connect": False
     }
