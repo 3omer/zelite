1b7e180fe181c1c10ccc84cd111801f2611464f8
OMAR <omo9@mail.com>
2020-08-30 17:18:33 +0200

    fix config classes

--- app/config.py
+++ app/config.py
@@ -1,16 +1,5 @@
 import os
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-
-class Config(object):
-    SECRET_KEY = os.environ.get('SECCRET_KEY') or 'admin'
-
-    MONGODB_SETTINGS = {
-        "db": os.environ.get("MONGODB_NAME") or "manzel_v1_test",
-        "host": os.environ.get("MONGODB_URI"),
-        "connect": False
-    }
 
 class Config(object):
     DEBUG = False
@@ -18,7 +7,7 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECCRET_KEY') or 'admin'
 
     MONGODB_SETTINGS = {
-        "db": os.environ.get("MONGODB_NAME") or "manzel_v1_test",
+        "db": os.environ.get("MONGODB_NAME") or "smanzel_dev",
     }
 
 
