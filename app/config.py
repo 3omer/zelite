afc8bd6b66566c2d1da912fbfdcc45868588bc8a
OMAR <omo9@mail.com>
2020-10-29 19:49:20 +0200

    add MQTT host config

--- app/config.py
+++ app/config.py
@@ -10,11 +10,21 @@ class Config(object):
         "db": os.environ.get("MONGODB_NAME") or "smanzel_dev",
     }
 
+    MQTT_SETTINGS = {
+        "host": "127.0.0.1",
+        "port": 8000,
+    }
+
 
 class ProductionConfig(Config):
     MONGODB_SETTINGS = {
         "host": os.environ.get("MONGODB_URI"),
         "connect": False
+    }
+
+    MQTT_SETTINGS = {
+        "host": os.environ.get("MQTT_HOST"),
+        "port": os.environ.get("MQTT_PORT"),
     }
 
 class DevelopmentConfig(Config):
