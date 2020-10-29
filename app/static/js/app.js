5c7cf609cf190eb9756f9e028ed216970deed22e
OMAR <omo9@mail.com>
2020-10-29 22:20:30 +0200

    fix MQTT_TOPICS_LIST renamed to MQTT_CLIENT_TOPICS

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -117,7 +117,7 @@ $(document).ready(function () {
         console.log('---- connected ----');
         console.log("Subscribing to User's Topics: ");
         // smanzel/username/hub_name/port
-        MQTT_TOPIC_LIST.forEach(topic => {
+        MQTT_CLIENT_TOPICS.forEach(topic => {
             client.subscribe('smanzel/omer/+/+', { qos: 1, onFailure: console.log, onSuccess: console.log });
         })
     }
