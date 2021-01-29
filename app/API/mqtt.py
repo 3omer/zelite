a075f6ce7db6833fb941f7c7c3e1f41b968c8102
Omar M. Osman <omarosmandev@gmail.com>
2021-01-29 23:24:22 +0200

    change /mqtt/create to /mqtt

--- app/API/mqtt.py
+++ app/API/mqtt.py
@@ -1,29 +1,36 @@
 from flask import jsonify, request, abort, Response, redirect, url_for
-from flask_login import current_user, login_required
 from app import app
 from app.models import Device, User
+from flask_jwt_extended import jwt_required, get_jwt_identity
 from app.API.utils import validate_user_mqtt, is_mqtt_admin
 
-# api for mqtt authentication
-@login_required
-@app.route("/api/v1/mqtt/create", methods=["POST"])
+
+@app.route("/api/v1/mqtt", methods=["POST"])
+@jwt_required
 def mqtt_credentials():
     """create MQTT credentials """
-    username = request.form.get('username')
-    password = request.form.get('password')
-    
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+    user_id = get_jwt_identity()["id"]
+    current_user = User.get_by_id(user_id)
     if current_user:
         current_user.mqtt_username = username
         current_user.mqtt_password = password
         current_user.save()
-    return redirect(url_for("manager"))
+    return jsonify({"msg": "MQTT credentials saved",
+                    "credentials": {
+                        "mqttUsername": current_user.mqtt_username,
+                        "mqttPasword": current_user.mqtt_password
+                    }})
+
 
 @app.route("/api/v1/mqtt/auth", methods=["POST"])
 def authMqtt():
     username = request.form.get('username')
     password = request.form.get('password')
-    topic    = request.form.get('topic')
-    acc      = request.form.get('acc')
+    topic = request.form.get('topic')
+    acc = request.form.get('acc')
 
     user = User.get_by_mqtt_username(username)
     if is_mqtt_admin(username, password):
@@ -32,22 +39,24 @@ def authMqtt():
         return Response("", 200)
     return abort(400)
 
+
 @app.route("/api/v1/mqtt/superuser", methods=["POST"])
 def superuser():
     username = request.form.get('username')
     password = request.form.get('password')
 
-    if username == "admin" and password == "admin": # these are just default .. change them
+    if username == "admin" and password == "admin":  # these are just default .. change them
         return Response("", 200)
     return abort(400)
+
 
 @app.route("/api/v1/mqtt/acl", methods=["POST"])
 def acl():
 
     username = request.form.get('username')
-    topic    = request.form.get('topic')
+    topic = request.form.get('topic')
     clientid = request.form.get('clientid')
-    acc      = request.form.get('acc') # 1 == SUB, 2 == PUB
+    acc = request.form.get('acc')  # 1 == SUB, 2 == PUB
 
     if username == "admin":
         return Response("", 200)
@@ -56,5 +65,3 @@ def acl():
     if user and (topic in user.topics):
         return Response("", 200)
     return abort(400)
-    
-            
