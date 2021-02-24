3194314e49097214d28b81cdb890744577763406
Omar M. Osman <omarosmandev@gmail.com>
2021-02-24 20:08:13 +0200

    enable CORS

--- app/__init__.py
+++ app/__init__.py
@@ -2,16 +2,18 @@ from flask import Flask
 from flask_login import LoginManager
 from flask_jwt_extended import JWTManager
 from flask_mongoengine import MongoEngine
+from flask_cors import CORS
 from app.config import *
 
 
 app = Flask(__name__)
 
-
 if app.env == "development":
     app.config.from_object(DevelopmentConfig)
 else:
     app.config.from_object(ProductionConfig)
+
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 db = MongoEngine(app)
 login_manager = LoginManager(app)
