cc64f39c6c763018497bd34f39a3cb99b2735625
Omar M. Osman <omarosmandev@gmail.com>
2021-04-10 04:20:24 +0200

    validate Device submits

--- app/JSONSchemas.py
+++ app/JSONSchemas.py
@@ -29,3 +29,19 @@ class UserSchema(Schema):
     )
 
     mqtt_topics = fields.List(fields.Str, dump_only=True)
+
+
+class DeviceSchema(Schema):
+    key = fields.Str()
+    name = fields.Str(required=True, validate=validate.Length(min=2))
+    place = fields.Str(required=True, validate=validate.Length(min=2))
+    port = fields.Int(validate=[validate.Range(min=0)])
+    d_type = fields.Str(data_key="type", required=True, validate=[
+                        validate.OneOf(["switch", "sensor"])])
+    topic = fields.Str(
+        validate=[
+            validate.ContainsNoneOf(
+                ["+", "#", " ", "*"], error="Invalid MQTT topic structure. Follow the standards defined by MQTT specifications."),
+            lambda val: val.count("/") > 1
+        ]
+    )
