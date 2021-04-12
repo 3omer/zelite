94c0af5d65a0c4894216ede02f8a270da0e3e357
Omar M. Osman <omarosmandev@gmail.com>
2021-04-12 06:41:49 +0200

    send emails in a thread

--- app/mailService.py
+++ app/mailService.py
@@ -1,11 +1,16 @@
 # Remote Services Calls
 
+from threading import Thread
 import jwt
 from flask import url_for
 from flask_mail import Message
 from . import app
 from . import mail
 
+
+def send_async(app, msg):
+    with app.app_context():
+        mail.send(msg)
 
 def send_verification_mail(recipient, token):
     msg = Message('Zelite Account Verification', sender="admin@zelite.com",
@@ -15,7 +20,8 @@ def send_verification_mail(recipient, token):
     msg.body = 'Click the link below to activate your account ' + link
     msg.html = "<h2>Zelite Platform</h2><P>Welcome To Zelite IoT Platform Click activate to verify your account</p> \
     <a href={} >activate</a>".format(link)
-    mail.send(msg)
+    # mail.send(msg)
+    Thread(target=send_async, args=(app, msg)).start()
 
 
 def send_password_reset_mail():
