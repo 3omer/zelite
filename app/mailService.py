fda1d6ce730a4f5b5aaf93b718843287571de7b8
Omar M. Osman <omarosmandev@gmail.com>
2021-04-29 09:56:27 +0200

    use SEND_EMAIL config

--- app/mailService.py
+++ app/mailService.py
@@ -12,11 +12,16 @@ def send_async(app, msg):
     with app.app_context():
         mail.send(msg)
 
+
 def send_verification_mail(recipient, token):
+    link = url_for('verify_email', token=token, _external=True)
+    if app.config["SEND_EMAIL"] == False:
+        print("USER EMAIL CONFIRMATION LINK: ", link)
+        return
+
     msg = Message('Zelite Account Verification', sender="omarosmandev@gmail.com",
                   recipients=[recipient])
 
-    link = url_for('verify_email', token=token, _external=True)
     msg.body = 'Click the link below to activate your account ' + link
     msg.html = "<h2>Zelite Platform</h2><P>Welcome To Zelite IoT Platform Click activate to verify your account</p> \
     <a href={} >activate</a>".format(link)
